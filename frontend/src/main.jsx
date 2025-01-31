a5af980d8b5c2c3d48788ad357230ceb1f2f483f
sivasankar55 <sivasankar8875@gmil.com>
2025-01-31 12:17:01 +0530

    setting up routers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './AuthContext.jsx'
-
+import { AuthProvider } from './authContext.jsx'
+import ProjectRoutes from './Routes.jsx'
+import { BrowserRouter as Router  } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <App />
+    <Router>
+    <ProjectRoutes />
+    </Router>
     </AuthProvider>
 )
