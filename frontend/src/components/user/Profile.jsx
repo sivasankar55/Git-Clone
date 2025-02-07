5069748f42d6aa4fb0605798aff12c544a2a54d3
sivasankar55 <sivasankar8875@gmil.com>
2025-02-07 21:22:02 +0530

    profile logout function

--- frontend/src/components/user/Profile.jsx
+++ frontend/src/components/user/Profile.jsx
@@ -1,34 +1,36 @@
 import React, { useEffect, useState } from "react";
-import {  useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./profile.css";
 import Navbar from "../Navbar";
 import { UnderlineNav } from "@primer/react";
 import { BookIcon, RepoIcon } from "@primer/octicons-react";
 import HeatMapProfile from "./HeatMap";
+import { useAuth } from "../../authContext";
 
 const Profile = () => {
   const navigate = useNavigate();
-  const [userDetails, setUserDetails] = useState({
-    name: "Username",
-  });
+  const [userDetails, setUserDetails] = useState({ username: "username" });
+  const { setCurrentUser } = useAuth();
 
   useEffect(() => {
     const fetchUserDetails = async () => {
       const userId = localStorage.getItem("userId");
 
-      if(userId){
-        try{
-        const response = await axios.get(`http://localhost:3002/userProfile/${userId}`);
-
-        setUserDetails(response.data);
-      } catch(err){
-        console.error("Cannot fetch user details:", err);
+      if (userId) {
+        try {
+          const response = await axios.get(
+            `http://localhost:3002/userProfile/${userId}`
+          );
+          setUserDetails(response.data);
+        } catch (err) {
+          console.error("Cannot fetch user details: ", err);
+        }
       }
-    }
-  };
-   fetchUserDetails();
-  },[]);
+    };
+    fetchUserDetails();
+  }, []);
+
   return (
     <>
       <Navbar />
@@ -95,8 +97,7 @@ const Profile = () => {
         </div>
 
         <div className="heat-map-section">
-          <HeatMapProfile/>
-          
+          <HeatMapProfile />
         </div>
       </div>
     </>
